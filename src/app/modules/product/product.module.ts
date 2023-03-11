c0df8cec3cb9b2fb489b4be3bbc76505673b67f9
NoamMonederoCasa <noammome98@gmail.com>
2023-03-11 18:53:58 +0100

    listar productos

---
+++ src/app/modules/product/product.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MaterialModule } from '../shared/material.module';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { ProductComponent } from './product/product.component';
+
+
+
+
+@NgModule({
+  declarations: [
+    ProductComponent
+  ],
+  imports: [
+    CommonModule,
+    MaterialModule,
+    FormsModule,
+    ReactiveFormsModule,
+    FlexLayoutModule
+  ]
+})
+export class ProductModule { }
